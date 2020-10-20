e6dbe29e23ffcd6661a32335084650cc8df0e1ab
zonghua2016 <2280314971@qq.com>
2020-10-21 02:10:28 +0800

    更新index

--- src/renderer/components/Content/index.vue
+++ src/renderer/components/Content/index.vue
@@ -2,7 +2,7 @@
  * @Author       : tongzonghua
  * @Date         : 2020-07-13 00:06:24
  * @LastEditors  : tongzonghua
- * @LastEditTime : 2020-10-21 02:07:43
+ * @LastEditTime : 2020-10-21 02:10:19
  * @Email        : tongzonghua@360.cn
  * @Description  : 内容取
  * @FilePath     : /cli/QPic/src/renderer/components/Content/index.vue
@@ -34,6 +34,7 @@ export default {
 
 <style lang="less" scoped>
 .content {
+  padding: 20px;
   width: 100%;
 }
 </style>
